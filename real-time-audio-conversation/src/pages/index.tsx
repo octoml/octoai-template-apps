97e3687e4cb41f13ba4de98a4b321f7866387015
vanessahlyan <vyan@octoml.ai>
2023-08-29 13:22:51 -0400

    Remove unnecessary files

--- real-time-audio-conversation/src/pages/index.tsx
+++ real-time-audio-conversation/src/pages/index.tsx
@@ -3,21 +3,19 @@
 import { useWhisper } from "@rkimball/use-whisper";
 import { useState } from "react";
 
-// unclicked "text-sm font-semibold leading-6 text-gray-900"
-
 function Status({ name, status }) {
-    return (
-        <div className="sm:col-span-4">
-                <label
-                  htmlFor={name}
-                  className="block text-sm font-medium leading-6 text-gray-900"
-                >
-                  {name} {status ? "✅" : "❌"}
-                </label>
-                <div className="mt-2">
-                </div>
-        </div>
-    )
+  return (
+    <div className="sm:col-span-4">
+      <label
+        htmlFor={name}
+        className="block text-sm font-medium leading-6 text-gray-900"
+      >
+        {name} {status ? "✅" : "❌"}
+      </label>
+      <div className="mt-2">
+      </div>
+    </div>
+  )
 }
 
 function Output({ label, output }) {
@@ -30,10 +28,6 @@ function Output({ label, output }) {
   )
 }
 export default function Page() {
-  const config = {
-    language: "en",
-    task: "transcription",
-  };
 
   let [hasRecorded, setRecorded] = useState(false);
 
@@ -46,15 +40,11 @@ export default function Page() {
     startRecording,
     stopRecording,
   } = useWhisper({
-    apiUrl: "http://localhost:8000/v1/audio/",
-    // apiUrl: "https://whisperx-3s0r2oe0snpi.nimbus.octoml.ai/v1/audio/",
+    apiUrl: "https://whisper-demo-kk0powt97tmb.octoai.cloud/v1/audio/",
+    apiKey: "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IjNkMjMzOTQ5In0.eyJzdWIiOiJkNTg3ODYyOS05MmRiLTQxY2MtYjA3Ni04YjBlZmI5OTRkMmYiLCJ0eXBlIjoidXNlckFjY2Vzc1Rva2VuIiwidGVuYW50SWQiOiJlODllOTNkYi0yMzE1LTQxNWMtOWQ5Yi0zZDM0YmUwNGVkNDIiLCJ1c2VySWQiOiJmMGE2YzQ0MS1iNWE5LTQ0ODgtOGFmOC0wYTViOWIyZDI2ODAiLCJyb2xlcyI6WyJGRVRDSC1ST0xFUy1CWS1BUEkiXSwicGVybWlzc2lvbnMiOlsiRkVUQ0gtUEVSTUlTU0lPTlMtQlktQVBJIl0sImF1ZCI6IjNkMjMzOTQ5LWEyZmItNGFiMC1iN2VjLTQ2ZjYyNTVjNTEwZSIsImlzcyI6Imh0dHBzOi8vaWRlbnRpdHkub2N0b21sLmFpIiwiaWF0IjoxNjkzMzI4NzYzfQ.sLPkJSxAyPSVbqze4UmBO0tPmtGK7Bk5XAP9hbo26PSsfb9mbJzCZWqK89PAzaBb1KfAIXFf-sB0NdW7jB4ycaVQba5mAD6SvrMQD1GK8NAzSMW74BfyDN8K8O8_3v-NLFZqo3ZeCgLEV-mMobYBMmuW-CdjO6Z6Gooe1QmpqXOe6QhOMFTfQlrTJKi797VWUK-lOWu8TJ_B22yeEsiMoQCEa7v0qiFoVtJQrLlCYxOOf07YwyIoFt96InrPtRt5_5qSFr0NoHnwr0IsSvXIrD4HomoHgrVaBkAWt2RDT3bDOm-ELjdQDGnWYwvh276Etqvbw_N9hQrdOxFPVbAIHg",
     streaming: true,
     timeSlice: 1_000, // 1 second
   });
-
-  // if (recording) {
-  //   setRecorded(true)
-  // }
 
   return (
     <div className="space-y-10 divide-y divide-gray-900/10">
@@ -82,12 +72,6 @@ export default function Page() {
               onClick={() => startRecording()}>
               Start
             </button>
-            {/* <button
-              type="button"
-              className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              onClick={() => pauseRecording()}>
-              Pause
-            </button> */}
             <button
               type="button"
               className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
